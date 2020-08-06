c7ff1914622920d5ffce941b1c2cb2b69284a238
Angela-1 <ruoshui_engr@163.com>
2020-08-06 15:44:42 +0800

    chore: modify docker command

--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,7 @@
+# build a image
 # docker build . -t primer-vue
 
+# run the image
 docker run -it --rm --privileged=true \
     -v /home/doris/source/primer-vue:/data \
     -w /data -p 8080:8080 primer-vue sh
