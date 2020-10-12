16aabaf0d43e7ef630e88f83496ef5f627861864
Angela-1 <ruoshui_engr@163.com>
2020-10-12 16:59:30 +0800

    feat: add pagination component

--- packages/pagination/pagination.vue
+++ packages/pagination/pagination.vue
@@ -1,24 +1,269 @@
 <template>
-  <nav class="paginate-container" aria-label="Pagination">
+  <nav v-if="pageCount > 0" class="paginate-container" aria-label="Pagination">
     <div class="pagination">
-      <span class="previous_page" aria-disabled="true"> 上一页 </span>
-      <em aria-current="page">1</em>
-      <a href="#url" aria-label="Page 2">2</a>
-      <a href="#url" aria-label="Page 3">3</a>
-      <span class="gap">…</span>
-      <a href="#url" aria-label="Page 8">8</a>
-      <a href="#url" aria-label="Page 9">9</a>
-      <a href="#url" aria-label="Page 10">10</a>
-      <a class="next_page" rel="next" href="#url" aria-label="Next Page">
-        下一页
-      </a>
+      <template
+        v-for="pageNum in pageNums"
+        :key="'num' + pageNum.type + pageNum.num"
+      >
+        <template v-if="pageNum.type === PaginationType.Prev">
+          <template v-if="pageNum.disabled">
+            <span
+              class="previous_page"
+              :aria-disabled="pageNum.disabled"
+              aria-label="Previous"
+            >
+              Previous
+            </span>
+          </template>
+          <template v-else>
+            <a
+              rel="prev"
+              @click="onPageChange(pageNum.num)"
+              aria-label="Previous"
+              class="previous_page"
+            >
+              Previous
+            </a>
+          </template>
+        </template>
+        <template v-if="pageNum.type === PaginationType.Break">
+          <span class="gap">…</span>
+        </template>
+        <template v-if="pageNum.type === PaginationType.Num">
+          <template v-if="pageNum.selected">
+            <em aria-current="page">
+              {{ pageNum.num }}
+            </em>
+          </template>
+          <template v-else>
+            <a
+              @click="onPageChange(pageNum.num)"
+              :aria-label="'Page' + pageNum.num"
+            >
+              {{ pageNum.num }}
+            </a>
+          </template>
+        </template>
+        <template v-if="pageNum.type === PaginationType.Next">
+          <template v-if="pageNum.disabled">
+            <span
+              class="next_page"
+              :aria-disabled="pageNum.disabled"
+              aria-label="Next"
+            >
+              Next
+            </span>
+          </template>
+          <template v-else>
+            <a
+              rel="next"
+              @click="onPageChange(pageNum.num)"
+              aria-label="Next"
+              class="next_page"
+            >
+              Next
+            </a>
+          </template>
+        </template>
+      </template>
     </div>
   </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
+import { PaginationType, PaginstionModel } from '../types/pagination'
 export default defineComponent({
-  name: 'PrPagination'
+  name: 'PrPagination',
+  props: {
+    currentPage: {
+      type: Number,
+      default: 1
+    },
+    pageCount: {
+      type: Number,
+      default: 0
+    },
+    marginPageCount: {
+      type: Number,
+      default: 1
+    },
+    surroundingPageCount: {
+      type: Number,
+      default: 2
+    },
+    showPages: {
+      type: Boolean,
+      default: true
+    }
+  },
+  emits: ['page-change'],
+  setup(props, ctx) {
+    const current = ref(props.currentPage)
+    /**
+     * 生成翻页组件数据，函数直接从@primer/component复制过来
+     * https://github.com/primer/components/blob/master/src/Pagination/model.js
+     *
+     * @param pageCount 总页数
+     * @param currentPage 当前页
+     * @param showPages 是否显示数字页码
+     * @param marginPageCount 两边留的页码数
+     * @param surroundingPageCount 当前页两边留的页码数
+     */
+    const buildPaginationModel = (
+      pageCount: number,
+      currentPage: number,
+      showPages: boolean,
+      marginPageCount: number,
+      surroundingPageCount: number
+    ): PaginstionModel[] => {
+      const pages: PaginstionModel[] = []
+
+      if (showPages) {
+        const pageNums: number[] = []
+        const addPage = (n: number) => {
+          if (n >= 1 && n <= pageCount) {
+            pageNums.push(n)
+          }
+        }
+
+        // Start by defining the window of pages to show around the current page.
+        // If the window goes off either edge, shift it until it fits.
+        let extentLeft = currentPage - surroundingPageCount
+        let extentRight = currentPage + surroundingPageCount
+        if (extentLeft < 1 && extentRight > pageCount) {
+          // Our window is larger than the entire range,
+          // so simply display every page.
+          extentLeft = 1
+          extentRight = pageCount
+        } else if (extentLeft < 1) {
+          while (extentLeft < 1) {
+            extentLeft++
+            extentRight++
+          }
+        } else if (extentRight > pageCount) {
+          while (extentRight > pageCount) {
+            extentLeft--
+            extentRight--
+          }
+        }
+
+        // Next, include the pages in the margins.
+        // If a margin page is already covered in the window,
+        // extend the window to the other direction.
+        for (let i = 1; i <= marginPageCount; i++) {
+          const leftPage = i
+          const rightPage = pageCount - (i - 1)
+          if (leftPage >= extentLeft) {
+            extentRight++
+          } else {
+            addPage(leftPage)
+          }
+          if (rightPage <= extentRight) {
+            extentLeft--
+          } else {
+            addPage(rightPage)
+          }
+        }
+
+        for (let i = extentLeft; i <= extentRight; i++) {
+          addPage(i)
+        }
+
+        const sorted = pageNums
+          .slice()
+          .sort((a, b) => a - b)
+          .filter((item, idx, ary) => !idx || item !== ary[idx - 1])
+        for (let idx = 0; idx < sorted.length; idx++) {
+          const num = sorted[idx]
+          const selected = num === currentPage
+          if (idx === 0) {
+            if (num !== 1) {
+              // If the first page isn't page one,
+              // we need to add a break
+              pages.push({
+                type: PaginationType.Break,
+                num: 1
+              })
+            }
+            pages.push({
+              type: PaginationType.Num,
+              num,
+              selected
+            })
+          } else {
+            const last = sorted[idx - 1]
+            const delta = num - last
+            if (delta === 1) {
+              pages.push({
+                type: PaginationType.Num,
+                num,
+                selected
+              })
+            } else {
+              // We skipped some, so add a break
+              pages.push({
+                type: PaginationType.Break,
+                num: num - 1
+              })
+              pages.push({
+                type: PaginationType.Num,
+                num,
+                selected
+              })
+            }
+          }
+        }
+
+        const lastPage = pages[pages.length - 1]
+        if (
+          lastPage.type === PaginationType.Num &&
+          lastPage.num !== pageCount
+        ) {
+          // The last page we rendered wasn't the actual last page,
+          // so we need an additional break
+          pages.push({
+            type: PaginationType.Break,
+            num: pageCount
+          })
+        }
+      }
+
+      const prev: PaginstionModel = {
+        type: PaginationType.Prev,
+        num: currentPage - 1,
+        disabled: currentPage === 1
+      }
+      const next: PaginstionModel = {
+        type: PaginationType.Next,
+        num: currentPage + 1,
+        disabled: currentPage === pageCount
+      }
+      return [prev, ...pages, next]
+    }
+
+    const pageNums = computed(() => {
+      return buildPaginationModel(
+        props.pageCount,
+        current.value,
+        props.showPages,
+        props.marginPageCount,
+        props.surroundingPageCount
+      )
+    })
+
+    const onPageChange = (val: number): void => {
+      console.log('page change', val)
+
+      current.value = val
+      ctx.emit('page-change', current.value)
+    }
+
+    return {
+      pageNums,
+      onPageChange,
+      PaginationType
+    }
+  }
 })
 </script>
--- src/views/DesignPagination.vue
+++ src/views/DesignPagination.vue
@@ -2,7 +2,24 @@
   <the-component>
     <template #title> pagination </template>
     <template #content>
-      <pr-pagination></pr-pagination>
+      <h2>Basic example</h2>
+      <pr-pagination :page-count="15" :current-page="2"></pr-pagination>
+
+      <h2>Customizing which pages are shown</h2>
+      <pr-pagination
+        :page-count="20"
+        :current-page="10"
+        :margin-page-count="2"
+        :surrounding-page-count="1"
+      ></pr-pagination>
+
+      <h2>Previous/next pagination</h2>
+      Current page: {{ page }} / {{ pageCount }}
+      <pr-pagination
+        :page-count="10"
+        :show-pages="false"
+        @page-change="pageChange"
+      ></pr-pagination>
     </template>
   </the-component>
 </template>
@@ -18,6 +35,19 @@ export default defineComponent({
     TheComponent,
     PrPagination
   },
-  setup() {}
+  setup() {
+    const pager = reactive({
+      page: 1,
+      pageCount: 10
+    })
+    const pageChange = (val: number) => {
+      console.log('recive page', val)
+      pager.page = val
+    }
+    return {
+      ...toRefs(pager),
+      pageChange
+    }
+  }
 })
 </script>
