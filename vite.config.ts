82e888fa3fd5f79672aa835d19e83b32a485b90b
Angela-1 <ruoshui_engr@163.com>
2020-10-26 09:47:12 +0800

    feat: add hmr config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import type { ServerConfig } from 'vite'
 
 const config: ServerConfig = {
+  hmr: {
+    port: 10080
+  },
   port: 8080,
   optimizeDeps: {
     include: ['@primer/css', '@primer/octicons', 'dayjs']
