fe9196d202ce81c912f5af2a923f522b44bb3079
Angela-1 <ruoshui_engr@163.com>
2021-01-14 17:51:04 +0800

    chore: add publish script

---
+++ scripts/publish-to-npm.sh
@@ -0,0 +1,13 @@
+# 发布脚本，使用前要先登录
+
+# 修改版本
+# yarn version --new-version <version>
+# Creates a new version specified by <version>.
+
+# yarn version --major
+# yarn version --minor
+# yarn version --patch
+
+# 发布，必须加上 --access public 因为名称带有前缀
+
+npm publish --access public
