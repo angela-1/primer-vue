568271ec780a3e8e7beb81540af117090df6db23
Angela-1 <ruoshui_engr@163.com>
2021-02-02 15:06:17 +0800

    feat: bump vuedoc vite.config change

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite';
-import vitePluginVuedoc from 'vite-plugin-vuedoc';
+import vitePluginVuedoc, { vueDocFiles } from 'vite-plugin-vuedoc';
 import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
 
 export default defineConfig({
-  // plugins: [vitePluginVuedoc({
-  //   highlight: {
-  //     theme: 'one-light'
-  //   }
-  // }), vue()],
-  plugins: [vitePluginVuedoc({}), vue()],
+  plugins: [
+    vitePluginVuedoc({
+      highlight: {
+        theme: 'one-light'
+      }
+    }),
+    vue({
+      include: [...vueDocFiles]
+    })
+  ],
   server: {
     port: 8080,
     // hmr: {
